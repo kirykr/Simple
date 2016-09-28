a62de26ee43d7f6a52baaa8601b34da95e21fdfc
kiry <kirymeas@gmail.com>
2016-09-28 17:15:51 +0700

    fix error on welcome page added import links

--- app/Http/Controllers/ComputerController.php
+++ app/Http/Controllers/ComputerController.php
@@ -47,7 +47,7 @@ class ComputerController extends Controller {
 	 * @param Request $request
 	 * @return Response
 	 */
-	public function  (ComputerRequest $request)
+	public function  store(ComputerRequest $request)
 	{
   
 		$input = $request->all();
