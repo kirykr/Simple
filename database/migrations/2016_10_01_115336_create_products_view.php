eb475b2ecf3a28672493022856548dfe4154912a
kiry <kirymeas@gmail.com>
2016-10-02 17:20:16 +0700

    fix view products

--- database/migrations/2016_10_01_115336_create_products_view.php
+++ database/migrations/2016_10_01_115336_create_products_view.php
@@ -14,6 +14,7 @@ class CreateProductsView extends Migration
     {
         //
         DB::statement("CREATE VIEW products AS
+                        SELECT
                         computers.id as ID,
                         computers.`name`,
                         computers.qtyinstock,
