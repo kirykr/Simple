fd06a429ad9940fb80f12422a63465526d7cb6db
kiry <kirymeas@gmail.com>
2016-10-06 18:58:00 +0700

    adsfasdf

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,9 +18,9 @@
 
 
   {{-- <link rel="stylesheet" href="{{asset('css/dropzone.css')}}"> --}}
- {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script> --}}
-  {{-- <script src="{{asset('js/libs.js')}}"></script> --}}
-  <script src="{{asset('js/jquery.min.js')}}"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script src="{{asset('js/libs.js')}}"></script>
+  {{-- <script src="{{asset('js/jquery.min.js')}}"></script> --}}
   <script src="{{asset('js/jquery-ui.min.js')}}"></script>
   <script src="{{asset('js/bootstrap.js')}}"></script>
   
